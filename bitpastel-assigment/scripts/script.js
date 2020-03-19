600360c0f355c0b092a890db260f812187817262
codingKrills <pulkitsinghmail@gmail.com>
2020-03-19 16:17:40 +0530

    Add files via upload

---
+++ bitpastel-assigment/scripts/script.js
@@ -0,0 +1,54 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
+
+
+  function user1() {
+    var user = document.getElementById('username').value
+    var pass = document.getElementById('password').value
+    if (user == "test@email.com" && pass == "123456") {
+      window.open("dashboard.html");
+    } else {
+      alert('incorrect username or password')
+    }
+  }
